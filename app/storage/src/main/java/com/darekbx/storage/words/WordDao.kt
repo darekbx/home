63df0744e2692c894b14b74e4a733756a75aba20
darekbx <darek.baranczuk@gmail.com>
2024-02-14 18:00:39 +0100

    Add words

--- app/storage/src/main/java/com/darekbx/storage/words/WordDao.kt
+++ app/storage/src/main/java/com/darekbx/storage/words/WordDao.kt
@@ -11,9 +11,15 @@ interface WordDao {
     @Insert
     suspend fun addWord(wordDto: WordDto)
 
-    @Query("SELECT * FROM word_item")
+    @Query("SELECT * FROM word_item ORDER BY is_archived ASC")
     fun fetchWords(): Flow<List<WordDto>>
 
     @Query("DELETE FROM word_item WHERE id = :itemId")
     suspend fun deleteWord(itemId: Long)
+
+    @Query("UPDATE word_item SET is_archived = :isArchived WHERE id = :itemId")
+    suspend fun setArchived(itemId: Long, isArchived: Boolean)
+
+    @Query("UPDATE word_item SET checked_count = :count WHERE id = :itemId")
+    suspend fun setCount(itemId: Long, count: Int)
 }
--- app/words/src/main/java/com/darekbx/words/WordsActivity.kt
+++ app/words/src/main/java/com/darekbx/words/WordsActivity.kt
@@ -2,11 +2,14 @@ package com.darekbx.words
 
 import android.os.Bundle
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.Surface
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import com.darekbx.common.LauncherActivity
 import com.darekbx.common.ui.theme.HomeTheme
+import com.darekbx.words.ui.WordsScreen
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
@@ -14,9 +17,9 @@ class WordsActivity : LauncherActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            HomeTheme(isDarkTheme = false) {
-                Surface(modifier = Modifier.fillMaxSize()) {
-
+            HomeTheme(isDarkTheme = true) {
+                Surface(modifier = Modifier.fillMaxSize().background(Color.Black)) {
+                    WordsScreen()
                 }
             }
         }
